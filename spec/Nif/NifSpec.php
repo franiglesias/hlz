91892456e34e1c14c95359614ce16cb483fe01b0
Fran Iglesias <franiglesias@mac.com>
2018-05-25 00:35:40 +0200

    Example

--- spec/Nif/NifSpec.php
+++ spec/Nif/NifSpec.php
@@ -7,21 +7,48 @@ use PhpSpec\ObjectBehavior;
 
 class NifSpec extends ObjectBehavior
 {
-    public function it_can_not_have_more_than_max_chars()
+    public function it_should_not_have_more_than_max_chars()
     {
         $this->beConstructedWith('000000000T');
         $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_can_not_have_less_than_min_chars()
+    public function it_should_not_have_less_than_min_chars()
     {
         $this->beConstructedWith('0000000T');
         $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_can_not_end_with_a_number()
+    public function it_should_not_end_with_a_number()
     {
         $this->beConstructedWith('000000000');
         $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
     }
+
+    public function it_should_not_start_with_a_letter_other_than_xyz()
+    {
+        $this->beConstructedWith('A0000000T');
+        $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
+    }
+
+    public function it_should_not_have_letters_in_the_middle()
+    {
+        $this->beConstructedWith('000Z0000T');
+        $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
+    }
+
+    public function it_should_return_the_NIF_as_string()
+    {
+        $this->beConstructedWith('36101628T');
+        $this->value()->shouldBe('36101628T');
+    }
+    //private const CONTROL_DIGITS = 'trwagmyfpdxbnjzsqvhlcke';
+    //private const MAGIC_DIVISOR = 23;
+
+    public function it_should_be_a_valid_NIF_if_modulus_is_zero()
+    {
+        $this->beConstructedWith('00000001T');
+        $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
+    }
+
 }
--- src/Nif/Nif.php
+++ src/Nif/Nif.php
@@ -4,18 +4,36 @@ namespace Dojo\Nif;
 
 use InvalidArgumentException;
 
+/**
+ * Class Nif
+ * @package Dojo\Nif
+ */
 class Nif
 {
+    /** @var int */
+    private $value;
+
+    /**
+     * Nif constructor.
+     * @param string $nif
+     * @throws InvalidArgumentException
+     */
     public function __construct(string $nif)
     {
-        if (\strlen($nif) > 9) {
-            throw new InvalidArgumentException('Too many chars');
+        if (! preg_match('/^[0-9XYZ]\d{7,7}[A-Z]$/', $nif)) {
+            throw new InvalidArgumentException(sprintf('%s is malformed', $nif));
         }
-        if (\strlen($nif) < 9) {
-            throw new InvalidArgumentException('Too few chars');
+
+        $numberPart = substr($nif, 0, 8);
+        $modulus = (int) $numberPart % 23;
+        if ($modulus !== 0 && substr($nif, -1) === 'T') {
+            throw new InvalidArgumentException(sprintf('%s is invalid', $nif));
         }
-        if (preg_match('/\w$/', $nif)) {
-            throw new InvalidArgumentException('Last char cannot');
-        }
+        $this->value = $nif;
+    }
+
+    public function value()
+    {
+        return $this->value;
     }
 }
